2bbcad05461967d4bf61577a8ae914b33651e2de
Rakhshona <rahshonakarimova@gmail.com>
2022-08-08 13:38:45 +0500

    Task2

--- task2.py
+++ task2.py
@@ -1,1 +1,8 @@
-print('How are you?')
+t = int(input('Enter which time in seconds do you want to transform to hours, minutes and seconds:'))
+h = t // 3600
+s = t % 60
+m = ((t - t % 60) // 60) - h * 60
+print(f'{h:02}:{m:02}:{s:02}')
+
+
+
